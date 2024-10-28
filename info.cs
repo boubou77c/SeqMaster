6144987fa637e9ffe4fe16b82141bd20e3ea6e1d
boubou77c <142178273+boubou77c@users.noreply.github.com>
2024-10-28 16:27:38 +0100

    Add files via upload

---
+++ info.cs
@@ -0,0 +1,8 @@
+Project: SeqMaster
+Version: 1.0.0
+Description: Application designed to calculate and view in a plot mathematical sequences
+Author: Boubou
+Nationality : France
+Discord : .bou_bou_
+GitHub : https://github.com/boubou77c/SeqMaster
+Creation data: October 2024
